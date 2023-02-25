b727fd4f545d44e194c8e7e225574c0bdf22e5b2
nabetse00 <nabetse00@users.noreply.github.com>
2023-02-25 01:06:37 +0000

    deploy: 6a579836716786ec0d6b12c9a9a7bc314792d62d

--- assets/NoAccount-1024257e.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-486d3cb4.js";import{c as e}from"./ui-276e3024.js";function c(o){return t.jsx(e,{type:"error",message:"No account connected",description:"Please connect to your wallet on "+o.network})}export{c as N};
